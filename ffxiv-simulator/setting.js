b8c83feb0c69981e1a64ed5aae6c7e026e848c81
Eroleice <yuhuisun940531@gmail.com>
2018-06-22 16:54:06 +0800

    update

--- ffxiv-simulator/setting.js
+++ ffxiv-simulator/setting.js
@@ -2,26 +2,30 @@
 
     set: function () {
 
-        var job = 'ast';		//职业
+        var job = 'ast';		// 职业
 
         var status = {
-            'wd': 135,			//武器性能
-            'ap': 2544,			//攻击力or魔法攻击力
-            'crt': 1047,		//暴击率
-            'det': 952,			//信念
-            'ss': 1366,			//技能速度or咏唱速度
-            'dh': 364,			//直击
-            'ten': 364			//韧性
+            'wd': 135,			// 武器性能
+            'ap': 2544,			// 攻击力or魔法攻击力
+            'crt': 1047,		// 暴击率
+            'det': 952,			// 信念
+            'ss': 1366,			// 技能速度or咏唱速度
+            'dh': 364,			// 直击
+            'ten': 364,			// 韧性
+            'mp': 20000         // 蓝量
         };
 
         var simulate = {
-            duration: 24000,	//模拟木桩时间，单位毫秒
-            times: 10,			//模拟次数
-            expect_mode: true,	//期望值模式
-            battle_log: false	//输出战斗记录
+            duration: 24000,	// 模拟木桩时间，单位10毫秒 (秒数x100)
+            times: 10,			// 模拟次数
+            expectMode: true,	// 期望值模式
+            battleLog: false,	// 输出战斗记录，开启情况下只能进行单次模拟
+            partyBuff: false    // 团队辅助buff
         };
 
         var opener = ['Cleric Stance', 'Potion', 'Combust II'];
+
+        simulate.times = (simulate.battleLog === true) ? 1 : simulate.times;  // 输出战斗记录时仅进行单次模拟
 
         return {
             'job': job,
--- ffxiv-simulator/sim.js
+++ ffxiv-simulator/sim.js
@@ -9,54 +9,87 @@
         // 玩家参数，实时修改
         this.player = {
             'status': data.status,            // 玩家属性
-            'buff': [],                       // 玩家BUFF
+            'source': {
+                'mp': data.status.mp,         // 玩家MP
+                'tp': 1000,                   // 玩家TP
+                'specialSource1': 0,          // 职业资源1
+                'specialSource2': 0           // 职业资源2
+            },
+            'buff': [],                       // 玩家buff && 目标debuff
+            'dot': [],                        // 玩家dot
             'cd': [],                         // 玩家技能冷却
             'stance': [],                     // 玩家姿态
             'tick': {
                 'animation': 0,               // 动作僵直
                 'gcd': 0,                     // 公共冷却
-                'cast': 0                     // 读条
+                'cast': 0,                    // 读条
+                'aa': 0                       // 普攻间隔
             },
+            'engage': false                   // 进入战斗状态
         };
         // 战斗参数，实时修改
         this.battle = {
             'opener': data.opener,            // 开场队列
             'time': data.simulate.duration,   // 战斗计时
-            'engage': false,                  // 战斗开始
-            'tick': 1500,                     // 服务器tick
+            'tick': 150,                      // 服务器tick
             'log': []                         // 战斗记录
         };
         // 统计参数，不会修改
-        this.statistic = {
-            'skill': [],                      // 技能顺序
-            'damage': [],                     // 技能伤害
-            'dps': 0                          // dps
-        };
+        this.statistic = [];
         // 配置文件
         this.rotation = require(this.initRotation(data.job));
     }
 
     start() {
-        /*
+
         while (this.battle.time > 0) {
-            console.log(this.battle.time);
-            this.battle.time -= 1;
+
+            // 如果玩家不在读条&&不在动作僵直，进入判定
+            if (this.player.tick.cast <= 0 && this.player.tick.animation <= 0) {
+
+
+     
+            }
+
+            tick(); // 所有时间结算，进入下一次循环验算
         }
-        */
-        this.player = test(this.player);
-        console.log(this.player.cast);
+
     }
 
+    // 读取输出循环配置文件
     initRotation(job) {
         return './rotation/' + job + '.js';
     }
 
+    // 所有时间-1毫秒
+    tick() {
+        this.battle.tick -= 1;
+        this.battle.time -= 1;
+        this.allTick(this.player.tick);
+        this.allTick(this.player.buff);
+        this.allTick(this.player.debuff);
+        this.allTick(this.player.cd);
+    }
+
+    allTick(arr) {
+        for (var k in arr) {
+            arr[k] -= 1;
+        }
+    }
+
+    // 更新class
+    updateFight(data) {
+        this.player = data.player;
+        this.battle = data.battle;
+        this.statistic = data.statistic;
+    }
+
+    // 输出循环判定
+    doRotation(data) {
+        this.updateFight(this.rotation.logic(data));
+    }
 }
 
-function test(player) {
-    player.cast = 10000;
-    return player;
-}
 module.exports = {
     'run': function (data) {
         var fight = new Fight(data);    // 建立新的战斗模型
